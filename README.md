567588c0a73f12ecd8535a5dc2be441aa99d0223
Kevin <kevin.ck.nguyen@gmail.com>
2022-03-27 21:47:20 -0700

    Ni!

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 "# Python_Comprehensive" 
 
 Repository to contain code that helps refamiliarize myself with concepts present in Python. Main topics to address include standard data types in python, diction for universal concepts, and Python specific methods.
+
+
+Order of Reading:
+1)Start.py
+2)Fundamentals Folder
+3)OOP Techniques Folder
+4)Flask
+5)MySql Integration
+6) Ajax integration
+6)Bringing it all together
+
+
+Bookmark: Data types
---
+++ Start.py
@@ -0,0 +1,20 @@
+benefits= ["readability","libraries","community","ease of use","wide scope"]
+
+installation= {
+    1: "Install python from https://www.python.org/downloads/windows/",
+    2: "For here on use windows terminal or standard console. Test installation with 'python -V' ",
+}
+
+
+execution={
+    "Enter python shell":{
+        1:"Initialize shell with command 'python' ",
+        2:"Typing code directly into console",
+        3:"Exit with command 'exit()' "
+    },
+    "PREFERRED: Running files":"From console type: python fileName.py",
+}
+
+indent="4 spaces"
+
+print(execution)
