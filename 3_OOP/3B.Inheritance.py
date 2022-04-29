d8ef42b53e96b4901c98959c1e215c39fbba966a
Kevin <kevin.ck.nguyen@gmail.com>
2022-04-28 22:47:38 -0700

    Pop Team Epic

---
+++ 3_OOP/3B.Inheritance.py
@@ -0,0 +1,52 @@
+#USING THE PRIOR CLASS AS AN EXAMPLE
+
+class User:
+    species="Human"
+    all_peoples=[]
+    def __init__(self,name,email="Default message:No email provided"):
+        self.name=name
+        self.email=email
+        self.status="Alive"
+        User.all_peoples.append(self)
+
+    def talk(self,message):
+        print(self.name,"says",message)
+
+    def perish(self):
+        self.status="Dead"
+        
+    @classmethod
+    def all_people(cls):
+        for x in cls.all_peoples:
+            print(x.name)
+
+    @classmethod
+    def change_species(cls):
+        cls.species="Unknown"
+
+    @staticmethod
+    def definition():
+        print(f"Static methods organize code. Methods that are unrelated directly to a class,but is categorized under the class.  \n Essentially static methods can be used to place a method that you would primarily use for all instances of a specific class, but is not beholden to  the attributes or other methods of a class")
+
+
+#INHERITANCE
+
+class Runner(User):
+    #IN THE INIT INCLUDE EVERYTHING, INCLUDING THE TRAITS INHERITED FROM SUPER
+    def __init__(self,name,email,speed=0):
+        super().__init__(name,email)
+        self.speed=speed
+
+    def talk(self,message):
+        super().talk(message)
+        print(self.name,"says","I'm also a runner")
+
+
+kevin=Runner("Kevin","kevin.ck.nguyen@gmail.com",5)
+tim=User("Tim")
+
+
+
+kevin.talk("This is an example")
+
+tim.talk("Hello")
